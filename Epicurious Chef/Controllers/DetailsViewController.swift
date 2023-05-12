701951471451517c1dd794859b0c4ffa17097c71
Sulochana Madasekara <sulochana.dilu@gmail.com>
2023-05-12 01:54:46 -0700

    Created modifications in DetailsViewController page.

--- Epicurious Chef/Controllers/DetailsViewController.swift
+++ Epicurious Chef/Controllers/DetailsViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Sulo Mac on 5/9/23.
 //
-
 import UIKit
 
 class DetailsViewController: UIViewController {
@@ -21,7 +20,51 @@ class DetailsViewController: UIViewController {
         let label = UILabel()
         label.textColor = .label
         label.textAlignment = .left
-        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.font = .systemFont(ofSize: 30, weight: .regular)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let excerciseCategoryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 30, weight: .regular)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let excerciseEquipmentLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 30, weight: .regular)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let targetMuscleGroupsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 30, weight: .regular)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let repsAndStepsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 30, weight: .regular)
         label.lineBreakMode = .byWordWrapping
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -41,6 +84,10 @@ class DetailsViewController: UIViewController {
     private func setUpUI(){
         view.addSubview(exceriseImageView)
         view.addSubview(excerciseNameLabel)
+        view.addSubview(excerciseCategoryLabel)
+        view.addSubview(excerciseEquipmentLabel)
+        view.addSubview(targetMuscleGroupsLabel)
+        view.addSubview(repsAndStepsLabel)
         
         NSLayoutConstraint.activate([
             exceriseImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 75),
@@ -52,14 +99,33 @@ class DetailsViewController: UIViewController {
             excerciseNameLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
             excerciseNameLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
      
+            excerciseCategoryLabel.topAnchor.constraint(equalTo: excerciseNameLabel.bottomAnchor, constant: 5),
+            excerciseCategoryLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            excerciseCategoryLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
             
+            excerciseEquipmentLabel.topAnchor.constraint(equalTo: excerciseCategoryLabel.bottomAnchor, constant: 5),
+            excerciseEquipmentLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            excerciseEquipmentLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            
+            targetMuscleGroupsLabel.topAnchor.constraint(equalTo: excerciseEquipmentLabel.bottomAnchor, constant: 5),
+            targetMuscleGroupsLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            targetMuscleGroupsLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            
+            
+            repsAndStepsLabel.topAnchor.constraint(equalTo: targetMuscleGroupsLabel.bottomAnchor, constant: 5),
+            repsAndStepsLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            repsAndStepsLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
         ])
         
     }
     
     private func setValues(){
         exceriseImageView.image  = UIImage(named: "detail")
-        excerciseNameLabel.text = "E1 ergerg s"
+        excerciseNameLabel.text = "Excercise Name"
+        excerciseCategoryLabel.text = "Excercise Category"
+        excerciseEquipmentLabel.text = "Equipment Needed"
+        targetMuscleGroupsLabel.text = "Targeted Muscle Groups"
+        repsAndStepsLabel.text = "Reps And Steps"
     }
     
 }
